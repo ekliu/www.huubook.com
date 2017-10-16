6d06196736a7da1f36284a63e9776b31b68b2914
ekliu <ekliu@126.com>
2017-10-16 20:49:48 +0800

    test

--- app/bootstrap_cli.php
+++ app/bootstrap_cli.php
@@ -3,6 +3,9 @@
 use Phalcon\Di\FactoryDefault\Cli as FactoryDefault;
 use Phalcon\Cli\Console as ConsoleApp;
 
+
+
+
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/app');
 
