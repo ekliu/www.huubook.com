616fd806ae69f5d5afaa32c1834cb9b921f18c0d
ekliu <ekliu@126.com>
2017-10-19 12:14:06 +0800

    test

--- app/modules/frontend/controllers/IndexController.php
+++ app/modules/frontend/controllers/IndexController.php
@@ -10,11 +10,22 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        echo 'index';
+        echo 'fontend index';
     }
 
     public function testAction()
     {
+        $result = false;
+        if (!$result) {
+            $data = json_encode([
+                'code' => 200,
+                'massages' => 'false',
+                'data' => [1, 2, 34]
+            ]);
+            throw new \Exception($data);
+        }
+
+
         //db test
         $model = new BaseModel();
         $data = $model->table('member')->wyFirst();
@@ -26,11 +37,16 @@ class IndexController extends ControllerBase
         $res = $this->cache->get($cache_key);
         if (empty($res)) {
             $this->cache->save($cache_key, time(), 5); //秒
-        }else{
+        } else {
             print_r($res);
             echo "   cache OK  <br>";
         }
 
+    }
+
+    public function infoAction()
+    {
+        phpinfo();
     }
 
 }
